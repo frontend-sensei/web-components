1a79782c05e5b3820b4ff317c578c02d7485bd0f
Sensei <frontendsensei612@gmail.com>
2021-12-24 21:01:30 +0200

    Add Buttons

---
+++ vue/components/buttons/Button/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    :class="[
+      'btn',
+      btnVariantClass
+    ]"
+    v-on="$listeners"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+const ALLOWED_VARIANTS = [
+  'blue',
+  'green',
+
+  'outline-blue',
+  'outline-green',
+
+  'link-blue',
+  'link-green'
+]
+
+export default {
+  props: {
+    variant: {
+      type: String,
+      default: '',
+      validator(variant) {
+        return ALLOWED_VARIANTS.includes(variant)
+      }
+    }
+  },
+  computed: {
+    btnVariantClass() {
+      return `btn-${this.variant}`
+    }
+  }
+}
+</script>
